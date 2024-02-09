fd597ecfe663756208aef2d339cdf70c1c8d423d
just1103 <hyojuson1103@gmail.com>
2024-02-09 16:03:33 +0900

    refac: 파일이름 및 폴더 정리

--- CreateProject/CreateProject/App/SceneDelegate.swift
+++ CreateProject/CreateProject/App/SceneDelegate.swift
@@ -10,16 +10,21 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    let splashVC = SplashViewController()
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
         guard let windowScene = scene as? UIWindowScene else { return }
         window = UIWindow(windowScene: windowScene)
         window?.makeKeyAndVisible()
-        
+
         let navigationController = UINavigationController()
         navigationController.view.backgroundColor = .white
-        
-        window?.rootViewController = ViewController()
+
+        window?.rootViewController = splashVC
     }
 
 }
--- CreateProject/CreateProject/Domains/Intro/SplashViewController.swift
+++ CreateProject/CreateProject/Domains/Intro/SplashViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class SplashViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
